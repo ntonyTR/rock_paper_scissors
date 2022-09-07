34af9777879bda1541961c8e366f697035a4cf7f
Antony Torres <antonyetr@outlook.com>
2022-09-07 00:30:14 -0500

    Source code completed, the game is playable, added a score counter and final result alert, needs to update README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rock_paper_scissors
+
+PSEUDOCODE
+>Make a random choice of three options (0, 1, 2)
+>Assign each value to a RPS value (0 = Rock, 1 = Paper, 2 = Scissors)
+>Ask user to input a RPS value (prompt Rock, Paper, Scissors)
+>Convert user input to a neutral string ('rOcK' = 'Rock', 'PAPER' = 'Paper', 'ScIsSors' = 'Scissors')
+>Compare user's input with the computer's choice 
+>Determine who wins
+>Alert the result ('you won!')
--- script.js
+++ script.js
@@ -1,26 +1,62 @@
-let randomNumber = Math.floor((Math.random()* 3));
-
+let userChoice;
 let computerChoice;
+let roundResult;
+let userScore = 0;
+let compuScore = 0;
+let drawsScore = 0;
 
-if (randomNumber === 0) {
-    computerChoice = 'Rock';
-} else if (randomNumber === 1){
-    computerChoice = 'Paper';
-} else {
-    computerChoice = 'Scissors';
-}
-console.log(computerChoice)
-let userChoice = prompt('Rock, Paper, Scissors');
+game();
 
-let result;
-if (userChoice == computerChoice){
-    result = `${userChoice} is equals to ${computerChoice}, is a tie!`;
-} else if (userChoice == 'Rock' && computerChoice == 'Scissors' || userChoice == 'Paper' && computerChoice == 'Rock' || userChoice == 'Scissors' && computerChoice == 'Paper') {
-    result = `${userChoice} beats ${computerChoice}, you won!`;
-} else {
-    result = `${computerChoice} beats ${userChoice}, you lost!`;
+function getUserChoice() {
+    userChoice = prompt('Rock, Paper, Scissors');
+    userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
+    return userChoice;
 }
 
-console.log(`You chose ${userChoice}`);
-console.log(`Computer chose ${computerChoice}`);
-console.log(result);
+function getCompChoice() {
+    const randomNum = Math.floor((Math.random()* 3));
+    computerChoice = (randomNum === 0) ? 'Rock' : (randomNum === 1) ? 'Paper' : 'Scissors';
+    return computerChoice;
+}
+
+function playRound(player, computer) {
+    if (player === computer){
+        roundResult = `${player} is equal to ${computer}, is a tie!`;
+        drawsScore++;
+    } else if (player === 'Rock' && computer === 'Scissors' || player === 'Paper' && computer === 'Rock' || player === 'Scissors' && computer === 'Paper') {
+        roundResult = `${player} beats ${computer}, you won!`;
+        userScore++;
+    } else if (player === 'Rock' && computer === 'Paper' || player === 'Paper' && computer === 'Scissors' || player === 'Scissors' && computer === 'Rock') {
+        roundResult = `${computer} beats ${player}, you lost!`;
+        compuScore++;
+    } else {
+        alert(`Invalid value`);
+        roundResult = `Invalid value`;
+        compuScore++;
+    }
+    console.log(`User choice: ${player}\n\nComputer choice: ${computer}\n\n${roundResult}`);
+    alert(`User choice: ${player}\n\nComputer choice: ${computer}\n\n${roundResult}`);
+    return roundResult;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        getUserChoice();
+        getCompChoice();
+        playRound(userChoice, computerChoice);
+    }
+
+    console.log(`User Score: ${userScore}\nComputer Score ${compuScore}\n`);
+    alert(`User Score: ${userScore}\nComputer Score ${compuScore}\n`);
+
+    if (userScore > compuScore) {
+        console.log(`You win!!!! :D`);
+        alert(`You win!!!! :D`);
+    } else if (userScore < compuScore) {
+        console.log(`You lose :(`);
+        alert(`You lose :(`);
+    } else {
+        console.log(`Is a draw :/`);
+        alert(`Is a draw :/`);
+    }
+}
